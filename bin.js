58e1c9da0b669f3a18cc43361c505c1d5a21d803
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-16 18:21:55 +0200

    make a cli tool

---
+++ bin.js
@@ -0,0 +1,24 @@
+
+/*
+USAGE:
+
+  scuttlebutt            # read a message your feed in $PAGER
+  scuttlebutt > feed.txt # dump your feed, latest to earliest
+
+                         # post a message
+  scuttlebutt say 'message....'
+  echo message | scuttlebutt say
+
+  scuttlebutt --reverse  # show feed since start
+
+                         # show feed in chronolical order
+  scuttlebutt --gte start --lt end
+
+                         # view the feed of a given author
+  scuttlebutt --author HASH
+
+  scuttlebutt help       # display this message.
+
+*/
+
+
