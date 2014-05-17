6226ca2017bb028641517510f49956acdfd01fbd
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-17 16:59:20 +0200

    latest

--- index.js
+++ index.js
@@ -2,17 +2,47 @@ var pl = require('pull-level')
 var pull = require('pull-stream')
 var Feed = require('./feed')
 var para = require('pull-paramap')
+var Blake2s = require('blake2s')
+var varint = require('varstruct').varint
+var codec = require('./codec')
 
 var first = new Buffer([2])
 var last = new Buffer(41) //1 + 8 + 32
 last.fill(255)
 last[0] = 2
+var firstLatest = new Buffer([3])
+var lastLatest = new Buffer(33)
+lastLatest.fill(255)
+lastLatest[0] = 3
 
-module.exports = function (db) {
+
+function bsum (value) {
+  return new Blake2s().update(value).digest()
+}
+
+
+module.exports = function (db, keys) {
+
+  var feeds = {}
 
   return {
-    feed: function (keys) {
+    feed: function (id, keys) {
+      if('string' === typeof id)
+        id = new Buffer(id, 'hex')
+      if(id.public)
+        keys = id, id = bsum(keys.public)
       return Feed(db, keys)
+    },
+    latest: function () {
+      return pull(
+        pl.read(db, {gte: firstLatest, lte: lastLatest}),
+        pull.map(function (data) {
+          return {
+            key: codec.LatestKey.decode(data.key).id,
+            value: varint.decode(data.value)
+          }
+        })
+      )
     },
     createFeedStream: function (opts) {
       opts = opts || {}
@@ -20,7 +50,6 @@ module.exports = function (db) {
       return pull(
         pl.read(db, {gt: first, lte: last, keys: false}),
         para(function (key, cb) {
-          console.log('LOOKUP', key)
           db.get(key, cb)
         }),
         Feed.decodeStream()
