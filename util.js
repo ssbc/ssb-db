b84ee9cf8157c56c61f5703cae06e3403b30a959
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-18 15:41:58 +0200

    move reused stuff here

---
+++ util.js
@@ -0,0 +1,28 @@
+
+var Blake2s = require('blake2s')
+
+exports.groups = function (done) {
+  var n = 0, error = null, values = []
+  return function () {
+    var i = n++
+    var j = 1
+    return function (err, value) {
+      //should never happen
+      if(--j) return n = -1, done(new Error('cb triggered twice'))
+      if(err) return n = -1, done(error = err)
+      values[i] = value
+      if(--n) return
+      done(null, values)
+    }
+  }
+}
+
+exports.bsum = function (value) {
+  if('string' === typeof value)
+    return new Blake2s().update(value, 'utf8').digest()
+  if(Buffer.isBuffer(value))
+    return new Blake2s().update(value).digest()
+  if(!value)
+    return new Blake2s().digest()
+}
+
