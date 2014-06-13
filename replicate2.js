ed07cf575de8deee358914f435f8ca2a85460800
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-13 17:25:12 +0200

    simpler, better replication algorithm

---
+++ replicate2.js
@@ -0,0 +1,33 @@
+var pull = require('pull-stream')
+var many = require('pull-many')
+var cat  = require('pull-cat')
+var u    = require('./util')
+
+module.exports = function (sbs, opts, cb) {
+  if('function' === typeof opts)
+    cb = opts, opts = {}
+
+  var source = many()
+
+  //source: stream {id: hash(pubkey), sequence: latest}
+  //pairs, then {okay: true} to show you are at the end.
+  source.add(cat([sbs.latest(), pull.once({okay: true})]))
+
+  //sink: filter out metadata, and write the actual data.
+  var sink = pull(
+    pull.filter(function (data) {
+      if(data.author) return true
+      if(u.isHash(data.id) && u.isInteger(data.sequence)) {
+        source.add(sbs.createHistoryStream(data.id, data.sequence, opts.live))
+      }
+      if(data.okay)
+        source.cap()
+    }),
+    sbs.createWriteStream(cb)
+  )
+
+  return {
+    source: source,
+    sink: sink
+  }
+}
