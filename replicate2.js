2670691d74e442c9b4e9060929c1a7c2879ef488
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-16 15:46:01 +0000

    encode replication stream

--- replicate2.js
+++ replicate2.js
@@ -2,16 +2,20 @@ var pull = require('pull-stream')
 var many = require('pull-many')
 var cat  = require('pull-cat')
 var u    = require('./util')
+var codec = require('./codec')
+var pvstruct = require('pull-varstruct')
 
 module.exports = function (sbs, opts, cb) {
   if('function' === typeof opts)
     cb = opts, opts = {}
 
+  opts = opts || {}
+
   var source = many()
 
   //source: stream {id: hash(pubkey), sequence: latest}
   //pairs, then {okay: true} to show you are at the end.
-  source.add(cat([sbs.latest(), pull.once({okay: true})]))
+  source.add(cat([sbs.latest(), pull.once(1)]))
 
   //sink: filter out metadata, and write the actual data.
   var sink = pull(
@@ -20,14 +24,14 @@ module.exports = function (sbs, opts, cb) {
       if(u.isHash(data.id) && u.isInteger(data.sequence)) {
         source.add(sbs.createHistoryStream(data.id, data.sequence, opts.live))
       }
-      if(data.okay)
+      if(data === 1)
         source.cap()
     }),
     sbs.createWriteStream(cb)
   )
 
   return {
-    source: source,
-    sink: sink
+    source: pull(source, pvstruct.encode(codec)),
+    sink: pull(pvstruct.decode(codec), sink)
   }
 }
