bc2d3c43327f3ce1fb4c12aba9225c571ac8be79
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-08 00:51:58 -0700

    handle validation states better

--- validation.js
+++ validation.js
@@ -1,6 +1,7 @@
 
 var deepEqual = require('deep-equal')
 var pull = require('pull-stream')
+var contpara = require('continuable-para')
 
 // make a validation stream?
 // read the latest record in the database
@@ -13,82 +14,114 @@ function clone (obj) {
   return o
 }
 
-module.exports = function (db, opts) {
+function get (db, key) {
+  return function (cb) {
+    return db.get(key, cb)
+  }
+}
+
+module.exports = function (ssb, opts) {
 
   var hash = opts.hash
-  var empty = opts.hash(new Buffer(0))
-  var zeros = new Buffer(empty.length)
+  var zeros = opts.hash(new Buffer(0))
   zeros.fill(0)
 
   var verify = opts.verify
   var encode = opts.encode
 
+  var validators = {}
+
+  function validateSync (msg, prev, pub) {
+    if(prev) {
+      if(!deepEqual(msg.prev, hash(encode(prev)))
+        && msg.sequence === prev.sequence + 1
+        && msg.timestamp > prev.timestamp)
+        return false
+    }
+    else {
+      if(!deepEqual(msg.prev, zeros)
+        && data.sequence === 1
+        && data.timestamp > 0)
+        return false
+    }
+    if(!deepEqual(msg.author, hash(pub.public || pub)))
+      return false
+
+    var _msg = clone(msg)
+    delete _msg.signature
+    return verify(pub, msg.signature, hash(encode(_msg)))
+  }
+
+  function createValidator (id) {
+
+    var queue = [], batch = [], prev, pubKey, cbs = []
+
+    return function (msg, cb) {
+
+      // INIT: retrive the latest message for the given id.
+      // when not in INIT state, queue any new messages.
+      // DRAIN: validate all queued messages.
+      // writes all valid message.
+      // after writing, if there are more queued queued messages
+      // goto DRAIN
+
+      if(!queue.length && !batch.length) {
+        queue.push({msg: msg, cb: cb})
+
+        console.log(ssb)
+        var lastDB = ssb.sublevel('lst')
+
+        contpara(
+          get(ssb, msg.prev),
+          ssb.getPublicKey(msg.author),
+          get(lastDB, msg.author)
+        )(function (err, results) {
+          prev = err ? null :results[0]
+          //get PUBLIC KEY out of FIRST MESSAGE.
+          pub = err ? msg.message : results[1]
+          console.log(results)
+          if((err ? 1 : results[2] + 1) !== msg.sequence)
+            return cb(new Error('sequence out of order'))
+
+          drain()
+        })
+
+      }
+      else
+        queue.push({msg: msg, cb: cb})
+
+      function drain () {
+        while(queue.length) {
+          var e = queue.shift()
+          cbs.push(e.cb)
+          console.log(e.msg, prev, pub)
+          validateSync(e.msg, prev, pub)
+          batch.push({
+            key: hash(encode(e.msg)), value: msg, type: 'put'
+          })
+          prev = e.msg
+        }
+
+        ssb.batch(batch, function (err) {
+          batch = []; while(cbs.length) cbs.shift()(err)
+          if(queue.length) drain()
+          else delete validators[id.toString('base64')]
+        })
+      }
+    }
+  }
+
   var v
   return v = {
-    validate: function (msg, prev, pub) {
-      if(prev) {
-        if(!deepEqual(msg.prev, hash(encode(prev)))
-          && msg.sequence === prev.sequence + 1
-          && msg.timestamp > prev.timestamp)
-          return false
-      }
-      else {
-        if(!deepEqual(msg.prev, zeros)
-          && data.sequence === 1
-          && data.timestamp > 0)
-          return false
-      }
-      var _msg = clone(msg)
-      delete _msg.signature
-      return verify(pub, msg.signature, hash(encode(_msg)))
-    },
-    createValidateStream: function (db, id) {
+    validateSync: validateSync,
+    validate: function (msg, cb) {
 
-      var latest = null, pub
+      var id = msg.author
+      var ids = id.toString('base64')
+      var validator = validators[ids] =
+        validators[ids] || createValidator(id)
 
-      function getPub (cb) {
-        if(pub) cb(null, pub)
-        else db.getPublicKey(id, cb)
-      }
-
-      function getLatest (cb) {
-        if(latest === null) {
-          db.sublevel('lst').get(id, function (err, seq) {
-            if(err) return cb(err)
-            db.get([id, seq], function (err, hash) {
-              if(err) return cb(err)
-              db.get(hash, function (err, msg) {
-                latest = msg
-                cb(err, msg, hash)
-              })
-            })
-          })
-        }
-        else {
-          cb(null, msg)
-        }
-      }
-
-      return pull(
-        pull.mapAsync(function (data, cb) {
-          getLatest(function (err, prev, hash, pub) {
-            //assert that data comes after prev
-
-            if(data.sequence === 1) {
-              assert.ok(err)
-              assert.equal(data.sequence, 1)
-            }
-            else {
-            }
-
-            latest = data
-            db.put(hash(opts.encode(latest)), latest)
-          })
-
-        }),
-        pull.drain()
-      )
-
+      return validator(msg, cb)
     }
   }
 }
