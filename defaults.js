36b051c68a4f29c0beb6b88f028c931f52b8ab59
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-07 20:24:45 -0700

    inject all crypto

---
+++ defaults.js
@@ -0,0 +1,31 @@
+
+var Blake2s = require('blake2s')
+var crypto = require('crypto')
+var JSONB = require('json-buffer')
+var ecc = require('eccjs')
+
+var curve = ecc.curves.k256
+
+exports.hash = function (data) {
+  return new Blake2s().update(data).digest()
+}
+
+exports.generate = function () {
+  return ecc.restore(curve, crypto.randomBytes(32))
+}
+
+exports.verify = function (pub, sig, hash) {
+  return ecc.verify(curve, pub, sig, hash)
+}
+
+exports.sign = function (pub, hash) {
+  return ecc.sign(curve, pub, hash)
+}
+
+exports.encode = function (data) {
+  return JSONB.stringify(data)
+}
+
+exports.decode = function (data) {
+  return JSONB.parse(data)
+}
