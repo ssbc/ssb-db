8a1fd5c6c68283d9bb58c79ffda9e1cdb63b1315
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-08 00:53:23 -0700

    message

---
+++ message.js
@@ -0,0 +1,32 @@
+
+
+
+module.exports = function (opts) {
+
+  var zeros = opts.hash(new Buffer(0))
+  zeros.fill(0)
+
+  function sign (msg, keys) {
+
+    msg.signature =
+      opts.sign(keys, opts.hash(opts.encode(msg)))
+
+    return msg
+  }
+
+  function create (keys, type, content, prev) {
+    return sign({
+      prev: prev ? opts.hash(opts.encode(prev)) : zeros,
+      author: opts.hash(keys.public),
+      type: type,
+      message: content,
+      sequence: prev ? prev.sequence + 1 : 1,
+
+    }, keys)
+
+  }
+
+  create.sign = sign
+
+  return create
+}
