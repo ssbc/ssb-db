577668ba214f213682773332a8a92cf89481e3cf
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-08 19:04:18 -0700

    use better hashes

--- rewrite.js
+++ rewrite.js
@@ -5,14 +5,15 @@ var pl       = require('pull-level')
 var paramap  = require('pull-paramap')
 var bytewise = require('bytewise/hex')
 var replicate = require('./replicate2')
+var timestamp = require('monotonic-timestamp')
+
 //53 bit integer
 var MAX_INT  = 0x1fffffffffffff
-
 var encode = bytewise.encode
 
 module.exports = function (db, opts) {
 
-  var logDB = db.sublevel('log')
+  var logDB = db.sublevel('log', {valueEncoding: opts})
   var feedDB = db.sublevel('fd')
   var clockDB = db.sublevel('clk')
   var lastDB = db.sublevel('lst')
@@ -21,19 +22,20 @@ module.exports = function (db, opts) {
     return function (cb) { db.get(encode(key), cb) }
   }
 
-  worklog(db, logDB)
+//  worklog(db, logDB)
 
   var validation = require('./validation')(db, opts)
 
-  db.pre(function (op, add) {
+  db.pre(function (op, add, _batch) {
     var msg = op.value
     // index by sequence number
+    var hash = bytewise.decode(op.key)
     add({
-      key: encode([msg.author, msg.sequence]), value: op.key,
+      key: encode([msg.author, msg.sequence]), value: hash,
       type: 'put', prefix: clockDB
     })
     add({
-      key: encode([msg.timestamp, msg.author]), value: op.key,
+      key: encode([msg.timestamp, msg.author]), value: hash,
       type: 'put', prefix: feedDB
     })
     // index the latest message from each author
@@ -41,13 +43,19 @@ module.exports = function (db, opts) {
       key: encode(msg.author), value: msg.sequence,
       type: 'put', prefix: lastDB
     })
+
+    add({
+      key: encode(timestamp()), value: hash,
+      type: 'put', prefix: logDB
+    })
+
   })
 
   db.getPublicKey = function (id, cb) {
     function cont (cb) {
       clockDB.get(encode([id, 1]), function (err, hash) {
       if(err) return cb(err)
-       db.get(hash, function (err, msg) {
+        db.get(encode(hash), function (err, msg) {
           if(err) return cb(err)
           cb(null, msg.message)
         })
@@ -74,7 +82,7 @@ module.exports = function (db, opts) {
     return pull(
       pl.read(clockDB, opts),
       paramap(function (key, cb) {
-        db.get(key, cb)
+        db.get(encode(key), cb)
       })
     )
 
@@ -103,7 +111,7 @@ module.exports = function (db, opts) {
         keys: false
       }),
       paramap(function (key, cb) {
-        db.get(key, cb)
+        db.get(encode(key), cb)
       })
     )
   }
