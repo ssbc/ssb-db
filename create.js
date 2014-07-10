862e0f3a72c504889325b6f465f7423857ab207c
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-09 20:13:43 -0700

    make it easy to create a database

---
+++ create.js
@@ -0,0 +1,8 @@
+
+var level = require('level')
+var sublevel = require('level-sublevel')
+var opts = require('../defaults')
+var SSB = require('./')
+module.exports = function (path, db) {
+  return SSB(sublevel(level(path, {valueEncoding: opts})), opts)
+}
