007dfd08441247bc7d5784a243607a068b14008c
Dominic Tarr <dominic.tarr@gmail.com>
2014-07-30 00:05:29 -0700

    dump should parse keys

--- dump.js
+++ dump.js
@@ -1,8 +1,17 @@
 var codec = require('./codec')
+var bytewise = require('bytewise/hex')
 
 var db = require('level')(process.argv[2], {
     keyEncoding: 'utf8', valueEncoding: codec
   })
 
+function decodeKey (key) {
+  key = key.split('\xff').filter(Boolean)
+  var _key = key.pop()
+  return [key, bytewise.decode(_key)]
+}
+
 db.createReadStream()
-  .on('data', console.log)
+  .on('data', function (op) {
+    console.log({key:decodeKey(op.key), value: op.value})
+  })
