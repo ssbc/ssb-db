84905c04cd9881cd7d156e81c0dac3df33b1529f
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-06 00:18:15 -0400

    document the defaults!

--- defaults.js
+++ defaults.js
@@ -16,23 +16,43 @@ var curve   = ecc.curves.k256
 
 module.exports = {
 
+  //this must return a buffer digest.
   hash: function (data, enc) {
     return new Blake2s().update(data, enc).digest()
   },
 
   keys: {
+    //this should return a key pair:
+    // {public: Buffer, private: Buffer}
+
     generate: function () {
       return ecc.restore(curve, crypto.randomBytes(32))
     },
+    //takes a public key and a hash and returns a signature.
+    //(a signature must be a node buffer)
     sign: function (pub, hash) {
       return ecc.sign(curve, pub, hash)
     },
+
+    //takes a public key, signature, and a hash
+    //and returns true if the signature was valid.
     verify: function (pub, sig, hash) {
       return ecc.verify(curve, pub, sig, hash)
     },
+
+    //codec for keys. this handles serializing
+    //and deserializing keys for storage.
+    //in elliptic curves, the public key can be
+    //regenerated from the private key, so you only
+    //need to serialize the private key.
+    //in RSA, you need to remember both public and private keys.
+
+    //maybe it's a good idea to add checksums and stuff
+    //so that you can tell that this is a valid key when
+    //read off the disk?
     codec: {
-      decode: function (private) {
-        return ecc.restore(curve, private)
+      decode: function (buffer) {
+        return ecc.restore(curve, buffer)
       },
       encode: function (keys) {
         return keys.private
@@ -42,7 +62,9 @@ module.exports = {
     }
   },
 
+  // the codec that is used to persist into leveldb.
+  // this is the codec that will be passed to levelup.
+  // https://github.com/rvagg/node-levelup#custom_encodings
   codec: codec
-
 }
 
