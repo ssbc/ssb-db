48869b2a0c30b8d86501561615fe6a29531865d3
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-14 19:13:09 -0400

    tests for createLogStream

---
+++ test/log.js
@@ -0,0 +1,38 @@
+'use strict'
+var tape     = require('tape')
+var level    = require('level-test')()
+var sublevel = require('level-sublevel/bytewise')
+var pull     = require('pull-stream')
+
+module.exports = function (opts) {
+
+  tape('simple', function (t) {
+
+    var db = sublevel(level('test-ssb-log', {
+      valueEncoding: require('../codec')
+    }))
+
+    var ssb = require('../')(db, opts)
+
+    var feed = ssb.createFeed(opts.keys.generate())
+
+    feed.add('msg', 'hello there!', function (err, msg) {
+      if(err) throw err
+      pull(
+        ssb.createLogStream(),
+        pull.collect(function (err, ary) {
+          if(err) throw err
+          t.equal(ary.length, 2)
+          console.log(ary)
+          t.end()
+        })
+      )
+    })
+
+  })
+}
+
+
+if(!module.parent)
+  module.exports(require('../defaults'))
+
