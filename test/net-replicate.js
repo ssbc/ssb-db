0c5bac357492ac789cb8fd95b820920bf31e73ea
Dominic Tarr <dominic.tarr@gmail.com>
2014-08-26 09:52:45 -0500

    remember to close server

--- test/net-replicate.js
+++ test/net-replicate.js
@@ -26,46 +26,47 @@ module.exports = function (opts) {
 
   tape('simple replicate', function (t) {
 
-    var sbs1 = w.createDB('sbs-replicate1')
-    var sbs2 = w.createDB('sbs-replicate2')
+    var ssb1 = w.createDB('ssb-replicate1')
+    var ssb2 = w.createDB('ssb-replicate2')
+
+    ssb1.id = 1
+    ssb2.id = 2
 
     var cb1 = u.groups(done)
 
-    var f1 = w.init(sbs1, 5, cb1())
-    var f2 = w.init(sbs2, 4, cb1())
+    var f1 = w.init(ssb1, 1, cb1())
+    var f2 = w.init(ssb2, 0, cb1())
 
-    sbs2.follow(opts.hash(f1.public), cb1())
-    sbs1.follow(opts.hash(f2.public), cb1())
+    ssb2.follow(opts.hash(f1.public), cb1())
+    ssb1.follow(opts.hash(f2.public), cb1())
 
     var ary = [], n = 1
 
     function done (err) {
-      console.log('initialized')
-
       if(err) throw err
       var cb2 = u.groups(done2)
 
       var server = net.createServer(function (stream) {
         stream.on('data', console.log)
         stream
-          .pipe(toStream( replicate(sbs1, cb2()) ))
+          .pipe(toStream( replicate(ssb1, cb2()) ))
           .pipe(stream)
 
       }).listen(null, function () {
-
         var stream = net.connect(server.address().port)
-        stream.pipe(toStream( replicate(sbs2, cb2()) )).pipe(stream)
+        stream.pipe(toStream( replicate(ssb2, cb2()) )).pipe(stream)
       })
 
       function done2 (err) {
+        server.close()
         console.log('REPLICATED')
         if(err) throw err
         //now check that the databases have really been updated.
 
         var cbs = u.groups(next)
 
-        pull(sbs1.createFeedStream(), pull.collect(cbs()))
-        pull(sbs2.createFeedStream(), pull.collect(cbs()))
+        pull(ssb1.createFeedStream(), pull.collect(cbs()))
+        pull(ssb2.createFeedStream(), pull.collect(cbs()))
 
         function next (err, ary) {
           if(err) throw err
