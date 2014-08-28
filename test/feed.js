9f84e44eb5cf45a6b0be59a1216fdcbbdc7d2333
Paul Frazee <pfrazee@gmail.com>
2014-08-27 22:05:43 -0500

    added test for tail=true feed-stream (issue #14)

--- test/feed.js
+++ test/feed.js
@@ -30,6 +30,47 @@ module.exports = function (opts) {
     })
 
   })
+
+  tape('tail', function (t) {
+
+    var db = sublevel(level('test-ssb-feed2', {
+      valueEncoding: require('../codec')
+    }))
+
+    var ssb = require('../')(db, opts)
+
+    var feed = ssb.createFeed(opts.keys.generate())
+
+    console.log('add 1'); console.log('add 2');
+    var nDrains = 0, nAdds = 2;
+    feed.add('msg', 'hello there!', function (err, msg) {
+      if(err) throw err
+      function addAgain() {
+        feed.add('msg', 'message '+nDrains, function(err) {
+          nAdds++;
+          console.log('add', nAdds);
+          if (err) throw err;
+          if (nAdds > 7) {console.log('TIMEOUT'); throw 'Should have had 5 drains by now.';}
+        });
+      }
+      var int = setInterval(addAgain, 300);
+      pull(
+        ssb.createFeedStream({ tail: true }),
+        pull.drain(function (ary) {
+          nDrains++;
+          console.log('drain', nDrains)
+          if (nDrains == 5) {
+            t.assert(true);
+            t.end()
+            clearInterval(int);
+          }
+        })
+      )
+      addAgain();
+
+    })
+
+  })
 }
 
 
