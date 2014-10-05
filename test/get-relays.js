7c2521e309a9cf6f21e157943803e9b1c93e044a
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-05 18:49:21 -0200

    test get-relays

---
+++ test/get-relays.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var pull     = require('pull-stream')
+var tape     = require('tape')
+var server   = require('../server')
+var cont     = require('cont')
+var compare = require('typewiselite')
+
+function sort (ary) {
+  return ary.sort(function (a, b) {
+    return compare(a.id, b.id) || compare(a.address, b.address)
+  })
+}
+
+function all(stream, cb) {
+  pull(stream, pull.collect(cb))
+}
+
+
+module.exports = function (opts) {
+  var w = require('./util')(opts)
+
+  tape('create a ssb and retrive the relays for a given feed', function (t) {
+
+    var ssb = w.createDB('get-relays')
+
+    var alice = ssb.createFeed()
+    var bob = ssb.createFeed()
+    var carol = ssb.createFeed()
+
+    cont.series([
+      alice.add('relay', {type: 'relay', address: 'localhost:65000'}),
+      bob.add('relay', {type: 'relay', address: 'localhost:65001'}),
+      carol.add('relay', {type: 'relay', address: 'localhost:65002'}),
+      alice.add('flw', {$feed: bob.id, $rel: 'follow'}),
+      alice.add('flw', {$feed: carol.id, $rel: 'follow'}),
+      bob.add('flw', {$feed: alice.id, $rel: 'follow'})
+    ]) (function () {
+
+    cont.para([
+      function (cb) {
+        all(server.getRelays(ssb, alice.id), function (err, ary) {
+          t.deepEqual(sort(ary), sort([
+            {id: alice.id, address: 'localhost:65000'},
+            {id: bob.id, address: 'localhost:65001'},
+            {id: carol.id, address: 'localhost:65002'},
+          ]))
+          cb()
+        })
+      },
+      function (cb) {
+        all(server.getRelays(ssb, bob.id), function (err, ary) {
+          t.deepEqual(sort(ary), sort([
+            {id: alice.id, address: 'localhost:65000'},
+            {id: bob.id, address: 'localhost:65001'},
+          ]))
+          cb()
+        })
+      },
+      function (cb) {
+        all(server.getRelays(ssb, carol.id), function (err, ary) {
+          t.deepEqual(sort(ary), sort([
+            {id: carol.id, address: 'localhost:65002'},
+          ]))
+          cb()
+        })
+      }
+    ]) (function () {
+      t.end()
+    })
+
+    })
+  })
+}
+
+if(!module.parent) {
+  module.exports(require('../defaults'))
+}
