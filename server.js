2eee5643f52c70c562415392f287b4f7ee780568
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-08 01:45:02 -0200

    get pub servers from the feed, and connect to them randomly

--- server.js
+++ server.js
@@ -11,23 +11,21 @@ var join = require('pull-join')
 
 function getRelays (ssb, id, cb) {
   return join(
-      pull(
-        cat([
-          pull.values([{dest: id}]),
-          ssb.feedsLinkedFrom(id, 'follow')
-        ]),
-        pull.map(function (link) { return {key: link.dest}})
-      ),
-      pull(
-        ssb.messagesByType('relay'),
-        pull.map(function (msg) {
-          return {key: msg.author, value: msg.message.address}
-        })
-      ),
-      function (id, _, address) {
-        return {id: id, address: address}
-      }
-    )
+    pull(
+      ssb.feedsLinkedFrom(id, 'follow'),
+      pull.map(function (link) { return {key: link.dest}})
+    ),
+    pull(
+      ssb.messagesByType('pub'),
+      pull.map(function (msg) {
+        return {key: msg.author, value: msg.message.address}
+      })
+    ),
+    function (id, _, address) {
+      console.log('JON?', id, _, address)
+      return {id: id, address: address}
+    }
+  )
 }
 
 function all(stream, cb) {
@@ -41,46 +39,84 @@ function addr (opts) {
   return opts.host + ':' + opts.port
 }
 
+function upto (expected) {
+  var o = {}
+  for(var k in expected)
+    o[k] = expected[k].me + expected[k].recv
+  return o
+}
+
+
 var net = require('net')
 
 exports = module.exports = function (ssb, feed, opts) {
 
-  all(cat([
-    pull.values((opts.seeds || []).map(function (e) {
-      return {address: e, id: null}
-    })),
-    getRelays(ssb, feed.id)
-  ]), function (err, ary) {
+  var connecting = false, server
 
-    console.log(addr(opts), ary)
+  function connect () {
+    if(connecting) return
+    connecting = true
+    if(server.closed) return
+    //TODO rewrite this to be be a realtime dataset
+    //track relay servers, and follows.
+    //keep track of how much data you have received
+    //from them and when you last replicated.
+    all(cat([
+      pull.values((opts.seeds || []).map(function (e) {
+        return {address: e, id: feed.id}
+      })),
+      getRelays(ssb, feed.id)
+    ]), function (err, ary) {
 
-    //now replicate with a random relay.
-    var a = ary[~~(Math.random()*ary.length)]
-    if(!a || !a.address) return console.error('no one to replicate with')
-    var address = a.address
-    console.log(addr(opts), 'to:', addr(a.address))
-    stream = net.connect(a.address.port, a.address.host)
+      //now replicate with a random relay.
+      ary = ary.filter(function (e) {
+        return e.address.port !== opts.port || e.address.host !== opts.host
+      })
 
+      var a = ary[~~(Math.random()*ary.length)]
+      if(!a || !a.address) return console.error('no one to replicate with')
+      var address = a.address
+
+      console.log(addr(opts), 'to:', addr(a.address))
+      stream = net.connect(a.address.port, a.address.host)
+
+      //when there are new messages, replicate more often.
+      //when you replicate but didn't get anything replicate slower.
+
+      stream
+        .on('error', function (err) {
+          console.log(err)
+          setTimeout(connect, 1000 + Math.random() * 3000)
+        })
+        .pipe(toStream(feed.createReplicationStream({
+            //we probably want a progress bar to show how in sync we are.
+          }, function (err, sent, recv, expected) {
+            connecting = false
+            server.emit('replicated', upto(expected))
+            //TODO: something smarter than randomly waiting
+            setTimeout(connect, 1000 + Math.random() * 3000)
+          })))
+        .pipe(stream)
+
+    })
+
+  }
+
+  return server = net.createServer(function (stream) {
     stream
-      .pipe(toStream(feed.createReplicationStream({
-          progress: function (a, b) {
-            console.log(addr(opts), addr(address), a, b)
-          }
-        }, function (err) {
-          console.log('DONE', addr(opts))
+      .pipe(toStream(feed.createReplicationStream({},
+        function (err, sent, recv, expected) {
+          server.emit('replicated', upto(expected))
         })))
       .pipe(stream)
-
+  }).listen(opts.port, function () {
+    setTimeout(connect, 1000 + Math.random() * 3000)
+  })
+  .on('close', function () {
+    server.closed = true
   })
 
-  return net.createServer(function (stream) {
-    stream
-      .pipe(toStream(feed.createReplicationStream(function (err) {
-        console.log('replicated with client')
-      })))
-      .pipe(stream)
-  }).listen(opts.port)
-
 }
+
 
 exports.getRelays = getRelays
