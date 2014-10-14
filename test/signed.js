6c4eebc4c86c3b607c9fa283dedce4e581db1ceb
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-14 14:16:41 -0200

    test encode/decode of signed messages

---
+++ test/signed.js
@@ -0,0 +1,49 @@
+
+var tape = require('tape')
+var hexpp = require('hexpp')
+
+module.exports = function (opts) {
+
+  tape('encode and sign a message', function (t) {
+
+    var keys = opts.keys.generate()
+
+    var message = {
+      author: opts.hash(keys.public),
+      previous: opts.hash(''),
+      sequence: 5,
+      timestamp: 1239435349,
+      timezone: 120,
+      type: new Buffer('test'),
+      message: {
+        foo: {bar: {baz: true}}
+      }
+    }
+
+
+    console.log('message', message)
+    var buffer = opts.codec.encode(message)
+
+    var sig = opts.keys.sign(keys, opts.hash(buffer))
+
+    message.signature = sig
+
+    var buffer_msg = opts.codec.encode(message)
+
+
+    console.log(hexpp(buffer, {ascii: true}))
+    console.log(hexpp(buffer_msg, {ascii: true}))
+
+    console.log(opts.codec.decode(buffer))
+    console.log(opts.codec.decode(buffer_msg))
+
+    t.end()
+
+  })
+
+
+}
+
+
+if(!module.parent)
+  module.exports(require('../defaults'))
