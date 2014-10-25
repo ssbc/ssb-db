f7219f68ebd1cdbe31dc7255571adbd1238b2dc1
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-25 17:26:06 +0100

    test for #48

--- test/msg-encoding.js
+++ test/msg-encoding.js
@@ -5,7 +5,60 @@ var sublevel = require('level-sublevel/bytewise')
 var pull     = require('pull-stream')
 var jhb      = require('json-human-buffer')
 
+var codec    = require('../codec')
+var hexpp    = require('hexpp')
+
 module.exports = function (opts) {
+
+  var content = {
+      "type":"post",
+      "is":"text",
+      "text":"test",
+      "timezone":300,
+      "rebroadcasts":{
+        "$rel":"rebroadcasts",
+        "$msg":
+          new Buffer("1BHEHMwZlikXB3o1mg+fP3GVo/+Xb7p46u3rqt/hHkA=", 'base64'),
+        "$feed":
+          new Buffer("rbU6CvdwBXxO/fDyoKuRyKxmZYyeb5+l87R9XVkN8bs=", 'base64'),
+        "timestamp":1414078805677,
+        "timezone":300
+      }
+    }
+
+  var msg = {
+    author: new Buffer(32),
+    previous: new Buffer(32),
+    timestamp: Date.now(),
+    sequence: 10,
+    content: content
+  }
+
+  var signed = {}
+
+  for(var k in msg)
+    signed[k] = msg[k]
+
+  signed.signature = new Buffer(64)
+  signed.signature.fill(0xab)
+
+  tape('Message', function (t) {
+    var enc = codec.encode(msg)
+    console.log('**** Message ****')
+    console.log(hexpp(enc))
+    var o = codec.decode(enc)
+    t.deepEqual(o, msg)
+    t.end()
+  })
+
+  tape('Signed', function (t) {
+    var enc = codec.encode(signed)
+    console.log('**** Signed ****')
+    console.log(hexpp(enc))
+    var o = codec.decode(enc)
+    t.deepEqual(o, signed)
+    t.end()
+  })
 
   tape('known error case 1', function (t) {
 
@@ -14,9 +67,8 @@ module.exports = function (opts) {
     }))
     var ssb = require('../')(db, opts)
     var feed = ssb.createFeed(opts.keys.generate())
-    var testMsg = jhb.parse('{"type":"post","is":"text","text":"test","timezone":300,"rebroadcasts":{"$rel":"rebroadcasts","$msg":"1BHEHMwZlikXB3o1mg+fP3GVo/+Xb7p46u3rqt/hHkA=.base64","$feed":"rbU6CvdwBXxO/fDyoKuRyKxmZYyeb5+l87R9XVkN8bs=.base64","timestamp":1414078805677,"timezone":300}}')
 
-    feed.add(testMsg, function (err, msg, hash) {
+    feed.add(content, function (err, msg, hash) {
       if(err) throw err
       t.assert(!!msg)
       t.assert(!!hash)
