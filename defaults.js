2ffd510d940c49b840584456243bbdf6e5ce96f5
Dominic Tarr <dominic.tarr@gmail.com>
2014-11-18 21:14:33 +0100

    keys and signatures as tagged base64

--- defaults.js
+++ defaults.js
@@ -1,7 +1,6 @@
 var Blake2s = require('blake2s')
 var crypto  = require('crypto')
 var ecc     = require('eccjs')
-var JSONH   = require('json-human-buffer')
 
 var curve   = ecc.curves.k256
 
@@ -16,12 +15,27 @@ function isString(s) {
 }
 function isHash (data) {
   return isString(data) && /^[A-Za-z0-9\/+]{43}=\.blake2s$/.test(data)
-  //return Buffer.isBuffer(data) && data.length == 32
 }
 
-function toBuffer(hash) {
+function tag (key, tag) {
+  return key.toString('base64')+'.' + tag
+}
+
+function toBuffer(buf) {
+  if(buf == null) return buf
+  return new Buffer(buf.substring(0, buf.indexOf('.')), 'base64')
+}
+
+function hashToBuffer(hash) {
   if(!isHash(hash)) throw new Error('sign expects a hash')
-  return new Buffer(hash.substring(0, 44), 'base64')
+  return toBuffer(hash)
+}
+
+function keysToBuffer(key) {
+  return isString(key) ? toBuffer(key) : {
+    public: toBuffer(key.public),
+    private: toBuffer(key.private)
+  }
 }
 
 module.exports = {
@@ -38,19 +52,26 @@ module.exports = {
     // {public: Buffer, private: Buffer}
 
     generate: function () {
-      return ecc.restore(curve, crypto.randomBytes(32))
+      var d = ecc.restore(curve, crypto.randomBytes(32))
+      return {
+        public: tag(d.public, 'k256'),
+        private: tag(d.private, 'k256')
+      }
     },
 
     //takes a public key and a hash and returns a signature.
     //(a signature must be a node buffer)
     sign: function (keys, hash) {
-      return ecc.sign(curve, keys, toBuffer(hash))
+      return tag(
+        ecc.sign(curve, keysToBuffer(keys), hashToBuffer(hash)),
+        'blake2s.k256'
+      )
     },
 
     //takes a public key, signature, and a hash
     //and returns true if the signature was valid.
     verify: function (pub, sig, hash) {
-      return ecc.verify(curve, pub, sig, toBuffer(hash))
+      return ecc.verify(curve, keysToBuffer(pub), toBuffer(sig), hashToBuffer(hash))
     },
   },
 
@@ -59,10 +80,10 @@ module.exports = {
   // https://github.com/rvagg/node-levelup#custom_encodings
   codec: {
     decode: function (string) {
-      return JSONH.parse(string)
+      return JSON.parse(string)
     },
     encode: function (obj) {
-      return JSONH.stringify(obj, null, 2)
+      return JSON.stringify(obj, null, 2)
     },
     buffer: false
   }
