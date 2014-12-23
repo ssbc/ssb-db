95811ab8a7bf4049e80fba175403b954092cad39
Dominic Tarr <dominic.tarr@gmail.com>
2014-12-23 23:21:07 +1300

    rewrite validation using pull-streams, and handling case where you get old messages twice

--- validation.js
+++ validation.js
@@ -2,7 +2,10 @@
 
 var deepEqual = require('deep-equal')
 var pull = require('pull-stream')
+var pushable = require('pull-pushable')
+
 var contpara = require('cont').para
+var explain = require('explain-error')
 // make a validation stream?
 // read the latest record in the database
 // check it against the incoming data,
@@ -27,6 +30,7 @@ function isString (s) {
 module.exports = function (ssb, opts) {
 
   var lastDB = ssb.sublevel('lst')
+  var clockDB = ssb.sublevel('clk')
   var hash = opts.hash
   var zeros = undefined
       //opts.hash(new Buffer(0))
@@ -57,7 +61,7 @@ module.exports = function (ssb, opts) {
 
         return false
       }
-      if(msg.sequence === prev.sequence + 1 
+      if(msg.sequence === prev.sequence + 1
         && msg.timestamp < prev.timestamp) {
 
           validateSync.reason = 'out of order'
@@ -97,77 +101,101 @@ module.exports = function (ssb, opts) {
     return true
   }
 
-  function createValidator (id) {
+  function createValidator (id, done) {
 
-    var queue = [], batch = [], prev, pub, cbs = []
+    var queue = [], batch = [], prev = null, pub, cbs = []
+    var ready = false, init = false
+
+    var queue = pushable()
+
+    contpara(
+      ssb.getPublicKey(id),
+      get(lastDB, id)
+    )(function (err, results) {
+      //get PUBLIC KEY out of FIRST MESSAGE.
+      pub = err ? null : results[1]
+      var expected = err ? 0 : results[2]
+      if(!expected) {
+        ready = true
+        return validate()
+      }
+
+      get(clockDB, [id, expected]) (function (err, _prev) {
+        if(err) throw explain(err, 'this should never happen')
+        prev = _prev
+        validate()
+      })
+    })
+
+    var recent = {}
+
+    function validate() {
+      pull(
+        queue,
+        pull.asyncMap(function (op, cb) {
+          if(pub == null) pub = op.value.content.public
+
+          if(recent[op.key]) cb()
+          else if(prev && op.value.sequence <= prev.sequence) {
+            clockDB.get([id, op.value.sequence], function (err, _key) {
+              if(_key === op.key) {
+                //we already have this msg, just drop it.
+                op.cb(null, op.value, op.key); cb()
+              }
+              else {
+                //looks like a private key has been compromized.
+                err = explain(err,
+                  'private key for:' + id +
+                  ' has been compromized'
+                )
+
+                op.cb(err); cb(err)
+              }
+            })
+          }
+          else cb(null, op)
+        }),
+        pull.filter(Boolean),
+        pull.asyncMap(function (op, cb) {
+          if(validateSync(op.value, prev, pub)) {
+            recent[op.key] = true
+            prev = op.value
+            cb(null, op)
+          } else {
+            var err = new Error(validateSync.reason)
+            op.cb(err); cb(err)
+          }
+        }),
+        pull.through(function (op) {
+          console.log(op.key.substring(0, 4), op.value.sequence)
+        }),
+//TODO make batches work correctly with pull-window.
+//this seems to break because of
+//it doesn't always group correctly.
+//        pw.recent(10, 100),
+        pull.map(function (op) {
+          return [op]
+        }),
+        pull.asyncMap(function (ops, cb) {
+          ssb.batch(ops, function (err) {
+            ops.forEach(function (op) {
+              delete recent[op.key]
+              op.cb(err, op.value, op.key)
+            })
+            cb(null, ops)
+          })
+        }),
+        pull.drain(null, done)
+      )
+    }
 
     return function (msg, cb) {
-
-      // INIT: retrive the latest message for the given id.
-      // when not in INIT state, queue any new messages.
-      // DRAIN: validate all queued messages.
-      // writes all valid message.
-      // after writing, if there are more queued queued messages
-      // goto DRAIN
-
-      var msghash = hash(encode(msg))
-      if(!queue.length && !batch.length) {
-
-        queue.push({hash: msghash, msg: msg, cb: cb})
-        contpara(
-          get(ssb, msg.previous),
-          ssb.getPublicKey(msg.author),
-          get(lastDB, msg.author)
-        )(function (err, results) {
-          prev = err ? null : results[0]
-          //get PUBLIC KEY out of FIRST MESSAGE.
-          pub = err ? msg.content.public : results[1]
-          var expected = err ? 1 : results[2] + 1
-
-          if(expected != msg.sequence) {
-            queue.shift()
-            return cb(
-              new Error('sequence out of order, expected:'
-                + expected + ' got: ' + msg.sequence)
-            )
-          }
-
-          drain()
-        })
-
-      }
-      else
-        queue.push({hash: msghash, msg: msg, cb: cb})
-    }
-
-    function drain () {
-      while(queue.length) {
-        var e = queue.shift()
-        if(validateSync(e.msg, prev, pub)) {
-          cbs.push(e)
-          batch.push({
-            key: e.hash, value: e.msg, type: 'put'
-          })
-          prev = e.msg
-        }
-        else {
-          e.cb(new Error(validateSync.reason
-            + ' when attempting to validate: '
-            + JSON.stringify(e.msg, null, 2)
-          ))
-        }
-      }
-
-      ssb.batch(batch, function (err) {
-        while(cbs.length) {
-          var e = cbs.shift()
-          e.cb(err, e.msg, e.hash)
-        }
-        batch = [];
-        if(queue.length) drain()
-        else delete validators[id.toString('base64')]
+      queue.push({
+        key: hash(encode(msg)),
+        value: msg, type: 'put', cb: cb
       })
     }
+
   }
 
   var v
@@ -176,9 +204,8 @@ module.exports = function (ssb, opts) {
     validate: function (msg, cb) {
 
       var id = msg.author
-      var ids = id.toString('base64')
-      var validator = validators[ids] =
-        validators[ids] || createValidator(id)
+      var validator = validators[id] =
+        validators[id] || createValidator(id)
 
       return validator(msg, cb)
     }
