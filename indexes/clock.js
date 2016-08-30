b4920571bb3088eba8f6646a15b22973f3d68f99
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-30 23:31:55 +1200

    move createUserStream into clock index

--- indexes/clock.js
+++ indexes/clock.js
@@ -2,19 +2,21 @@ var ref    = require('ssb-ref')
 var path = require('path')
 var Follower = require('../follower')
 var pull = require('pull-stream')
+var ltgt = require('ltgt')
 //53 bit integer
 var MAX_INT  = 0x1fffffffffffff
 var u = require('../util')
 
 module.exports = function (db, opts) {
 
-  var indexPath = path.join(opts.path, 'clock')
+  var indexPath = path.join(db.location, 'clock')
   var index = Follower(db, indexPath, 1, function (data) {
     if(data.sync) return
     return {
       key: [data.value.author, data.value.sequence], value: data.key, type: 'put'
     }
   })
+
   index.createHistoryStream = function (opts) {
     var opts    = u.options(opts)
     var id      = opts.id
@@ -32,14 +34,31 @@ module.exports = function (db, opts) {
       }),
       db.lookup(opts.keys !== false, opts.values !== false)
     )
+  }
 
-//    return clockDB.createHistoryStream(opts)
+  index.createUserStream = function (opts) {
+    opts = u.options(opts)
+    //mutates opts
+    ltgt.toLtgt(opts, opts, function (value) {
+      return [opts.id, value]
+    }, u.lo, u.hi)
+    var keys = opts.keys, values = opts.values
+    opts.keys = false
+    opts.values = true
+    console.log(opts)
+    return pull(
+      index.read(opts),
+      db.lookup(keys, values)
+    )
   }
 
   return index
 
 
 }
+
+
+
 
 
 
