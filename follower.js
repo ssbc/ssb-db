f69d61a4556db507478203ca401330d849bba049
Dominic Tarr <dominic.tarr@gmail.com>
2016-09-01 16:32:42 +1200

    expose rebuild

--- follower.js
+++ follower.js
@@ -5,34 +5,17 @@ var Write = require('pull-write')
 var pl = require('pull-level')
 var defer = require('pull-defer')
 
-function create(path) {
-  return Level(path, {keyEncoding: bytewise, valueEncoding: 'json'})
-}
-
 module.exports = function (_db, path, version, map) {
-  var db = create(path)
+  var db = create(path), writer
 
   var META = '\x00', since
 
-  db.get(META, {keyEncoding: 'utf8'}, function (err, value) {
-    since = value && value.since || 0
-    if(err) // new database
-      next()
-    else if (value.version !== version) {
-      db.close(function () {
-        Level.destroy(path, function (err) {
-          if(err) throw err //just die?
-          db = create(path)
-          since = 0
-          next()
-        })
-      })
-    }
-    else
-      next()
-  })
+  var written = 0, waiting = [], closed
 
-  var written = 0, waiting = []
+  function create(path) {
+    closed = false
+    return Level(path, {keyEncoding: bytewise, valueEncoding: 'json'})
+  }
 
   function await(ready) {
     _db.seen.await(function () {
@@ -41,46 +24,84 @@ module.exports = function (_db, path, version, map) {
     })
   }
 
-  var reader, closed
+  function close (cb) {
+    closed = true
+    //todo: move this bit into pull-write
+    if(writer)
+      writer.abort(function () {
+        db.close(cb)
+      })
+    else db.close(cb)
+  }
 
-  function next () {
-    while(waiting.length && waiting[0].ts <= since) {
-      waiting.shift().cb()
+
+  function build (rebuild, cb) {
+    since = null
+    if(rebuild)
+      destroy()
+    else
+      db.get(META, {keyEncoding: 'utf8'}, function (err, value) {
+        since = value && value.since || 0
+        if(err) // new database
+          next()
+        else if (value.version !== version) destroy()
+        else
+          next()
+      })
+
+    function destroy () {
+      close(function () {
+        Level.destroy(path, function (err) {
+          if(err) throw err //just die?
+          db = create(path)
+          since = 0
+          next()
+        })
+      })
     }
 
-    pull(
-      reader = _db.createLogStream({gt: since, live: true, sync: false}),
-      Write(function (batch, cb) {
-        if(closed) return cb(new Error('database closed while index was building'))
-        db.batch(batch, function (err) {
-          if(err) return cb(err)
-          since = batch[0].value.since
-          //callback to anyone waiting for this point.
-          while(waiting.length && waiting[0].ts <= since) {
-            waiting.shift().cb()
-          }
-          cb()
-        })
-      }, function reduce (batch, data) {
-        if(data.sync) return batch
-        var ts = data.ts || data.timestamp
+    function next () {
+      while(waiting.length && waiting[0].ts <= since) {
+        waiting.shift().cb()
+      }
 
-        if(!batch)
-          batch = [{
-            key: META,
-            value: {version: version, since: ts},
-            valueEncoding: 'json', keyEncoding:'utf8', type: 'put'
-          }]
+      if(closed) return
 
-        batch = batch.concat(map(data))
-        batch[0].value.since = Math.max(batch[0].value.since, ts)
-        return batch
-      }, 512, function (err) {
-        //probably a fatal database error
-        if(err) console.error(err.stack)
-      })
-    )
+      pull(
+        _db.createLogStream({gt: since, live: true, sync: false}),
+        writer = Write(function (batch, cb) {
+          if(closed) return cb(new Error('database closed while index was building'))
+          db.batch(batch, function (err) {
+            if(err) return cb(err)
+            since = batch[0].value.since
+            //callback to anyone waiting for this point.
+            while(waiting.length && waiting[0].ts <= since) {
+              waiting.shift().cb()
+            }
+            cb()
+          })
+        }, function reduce (batch, data) {
+          if(data.sync) return batch
+          var ts = data.ts || data.timestamp
+
+          if(!batch)
+            batch = [{
+              key: META,
+              value: {version: version, since: ts},
+              valueEncoding: 'json', keyEncoding:'utf8', type: 'put'
+            }]
+
+          batch = batch.concat(map(data))
+          batch[0].value.since = Math.max(batch[0].value.since, ts)
+          return batch
+        }, 512, cb)
+      )
+    }
   }
+
+  build(null, function (err) {
+    //console.error(err)
+  })
 
   return {
     get: function (key, cb) {
@@ -89,6 +110,7 @@ module.exports = function (_db, path, version, map) {
         db.get(key, cb)
       })
     },
+    await: await,
     read: function (opts) {
       opts = opts || {}
       if(since === _db.seen.get()) return pl.read(db, opts)
@@ -99,19 +121,12 @@ module.exports = function (_db, path, version, map) {
       })
       return source
     },
-    close: function (cb) {
-      closed = true
-      //todo: move this bit into pull-write
-      if(reader)
-        reader(true, function () {
-          db.close(cb)
-        })
-      else db.close(cb)
-    }
+    rebuild: function (cb) {
+      build(true, cb)
+    },
+    close: close,
     //put, del, batch - leave these out for now, since the indexes just map.
   }
+
 }
-
-
-
 
