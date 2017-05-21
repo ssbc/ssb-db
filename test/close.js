b866f56d9821fc045b835ff970ea812575cc7ead
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-21 15:30:18 +1200

    check that the db closes correctly, including in legacy mode

---
+++ test/close.js
@@ -0,0 +1,43 @@
+
+'use strict'
+var tape     = require('tape')
+var level    = require('level-test')()
+var sublevel = require('level-sublevel/bytewise')
+var pull     = require('pull-stream')
+var ssbKeys  = require('ssb-keys')
+var createFeed = require('ssb-feed')
+
+
+tape('load', function (t) {
+  var create = require('ssb-feed/util').create
+
+  var db = sublevel(level('test-ssb-feed', {
+    valueEncoding: require('../codec')
+  }))
+
+  var ssb = require('../')(db, {})
+
+  ssb.createFeed().add({type:'whatever'}, function (err) {
+    if(err) throw err
+  //  t.end()
+    ssb.close(function () {
+      t.end()
+    })
+  })
+})
+tape('reopen', function (t) {
+  var db = sublevel(level('test-ssb-feed', {
+    valueEncoding: require('../codec')
+  }))
+
+  var ssb = require('../')(db, {})
+
+  pull(
+    ssb.createLogStream(),
+    pull.collect(function (err, ary) {
+      t.end()
+
+    })
+  )
+})
+
