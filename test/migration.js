cf04f4b8162e9ef0cb7956331e86604741715bbc
Dominic Tarr <dominic.tarr@gmail.com>
2017-06-07 10:52:13 +1200

    test migration code, with progress bars!

---
+++ test/migration.js
@@ -0,0 +1,91 @@
+var createFeed = require('ssb-feed')
+var ssbKeys = require('ssb-keys')
+var timestamp = require('monotonic-timestamp')
+var level = require('level')
+var sublevel = require('level-sublevel')
+var latest = {}, log = [], db
+
+var tape = require('tape')
+
+tape('generate fake feed', function (t) {
+  var start = Date.now()
+  var feed = createFeed({
+    getLatest: function (id, cb) {
+      cb(null, latest[id])
+    },
+    add: function (msg, cb) {
+      latest[msg.author] = {key: '%'+ssbKeys.hash(JSON.stringify(msg, null, 2)), value: msg}
+      log.push(msg)
+      cb()
+    }
+  }, ssbKeys.generate())
+
+  var l = 100000
+  while(l--)
+    feed.add({type: 'test', text:'hello1', l: l}, function () {})
+
+  console.log('generate', Date.now() - start)
+  t.end()
+})
+
+tape('populate legacy database', function (t) {
+  var start = Date.now()
+  db = sublevel(level('/tmp/test-ssb-feed_'+Date.now(), {
+    valueEncoding: require('../codec')
+  }))
+
+  require('../legacy')(db)
+
+  ;(function next () {
+    var batch = log.splice(0, 1000)
+    db.batch(batch.map(function (msg) {
+      var key = '%'+ssbKeys.hash(JSON.stringify(msg, null, 2))
+      return {
+        key: key,
+        value: {
+          key: key, value: msg, timestamp: +timestamp()
+        },
+        type: 'put'
+      }
+    }), function (err) {
+      if(log.length) {
+        console.log(log.length)
+        setTimeout(next)
+      }
+      else {
+        console.log('legacy-write', Date.now() - start)
+        t.end()
+      }
+    })
+  })()
+
+})
+
+tape('migrate', function (t) {
+  var start = Date.now()
+  var flume = require('../db')('/tmp/test-ssb-migration_'+Date.now())
+
+  var int = setInterval(function () {
+    console.log(flume.progress)
+  },100)
+
+  flume.ready(function (isReady) {
+    if(isReady) {
+      console.log('ready!', flume.since.value)
+      console.log(flume.progress)
+      console.log('migrate', Date.now() - start)
+      clearInterval(int)
+      t.equal(flume.progress.current, flume.progress.target)
+      t.equal(flume.progress.ratio, 1)
+      t.end()
+    }
+  })
+
+  require('../legacy')(db, flume)
+
+})
+
+
+
+
+
