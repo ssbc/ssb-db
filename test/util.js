e82f83b995b12b0c65bfae3c100af6dfc227f655
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-23 23:16:29 +1300

    check if we are in browser before loading rimraf and mkdirp

--- test/util.js
+++ test/util.js
@@ -4,8 +4,11 @@ module.exports = function createSSB (name, opts, keys) {
   opts = opts || {}
   var dir = require('path').join(require('osenv').tmpdir(), name)
   if (opts.temp !== false) {
-    require('rimraf').sync(dir)
-    require('mkdirp').sync(dir)
+    if(process.title == 'node') {
+      //don't do this if we are in the browser
+      require('rimraf').sync(dir)
+      require('mkdirp').sync(dir)
+    }
   }
   opts.keys = opts.keys || ssbKeys.generate()
   return require('../create')(dir, opts, opts.keys)
