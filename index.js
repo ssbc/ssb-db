b947cc1ed97b7acf27a63c45075443dcdb50cf47
Christian Bundy <christianbundy@fraction.io>
2019-12-20 12:31:26 -0800

    Add feed ID as del() usage param

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ module.exports = {
 
   //    usage                    : valid.sync(usage, 'string?|boolean?'),
       close                    : close,
-      del: valid.async(ssb.del, 'msgLink'),
+      del: valid.async(ssb.del, 'msgLink|feedId'),
       publish                  : valid.async(feed.add, 'string|msgContent'),
       add                      : valid.async(ssb.add, 'msg'),
       queue                      : valid.async(ssb.queue, 'msg'),
