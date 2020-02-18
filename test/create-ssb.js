0bb5c60b537692b261fcf38acaf833c6183efc0e
mixmix <whimful@gmail.com>
2020-02-18 14:12:12 +1300

    fix test/close.js ??

--- test/create-ssb.js
+++ test/create-ssb.js
@@ -1,12 +1,12 @@
 var ssbKeys = require('ssb-keys')
 
-module.exports = function createSSB (name, opts, keys) {
+module.exports = function createSSB (name, opts) {
   opts = opts || {}
   var dir = require('path').join(require('osenv').tmpdir(), name)
   if (opts.temp !== false) {
     require('rimraf').sync(dir)
     require('mkdirp').sync(dir)
   }
-  opts.keys = opts.keys || ssbKeys.generate()
-  return require('../create')(dir, opts, opts.keys)
+  var keys = opts.keys = opts.keys || ssbKeys.generate()
+  return require('../create')(dir, opts, keys)
 }
