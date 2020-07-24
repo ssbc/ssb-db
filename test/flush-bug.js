4c8e1f1a6b869412e2ac2e48c7624a6caa94c40d
Christian Bundy <christianbundy@fraction.io>
2020-07-24 10:17:49 -0700

    Fix flush() bug
    Problem: The Async-Write module that we were using doesn't handle
    batches larger than the buffer, and is a generalized tool that requires
    having two buffers: `state.queue` for SSB-Validate and `queue.buffer`
    for Async-Write.
    
    Solution: Remove the Async-Write module and write simple logic that
    queues writes and doesn't lose messages in the process. This
    implementation is simpler, writing whenever we have the option to do so,
    and gives us similar performance without hardcoding buffer sizes or
    timer intervals. Running the `04-minimal.js` test from Bench-SSB took
    buffer sizes and timer intervals. Running `04-minimal.js` from Bench-SSB
    took 13.341 s ± 0.281 s before this change and 13.368 s ± 0.146 s after.

--- test/flush-bug.js
+++ test/flush-bug.js
@@ -1,5 +1,6 @@
 const tape = require('tape')
 const createSsb = require('./util/create-ssb')
+const { promisify } = require('util')
 
 // If you write faster than the database can save messages, it adds them to
 // some kind of queue, which eventually gets 'flushed'. Unfortunately it looks
@@ -9,11 +10,21 @@ const createSsb = require('./util/create-ssb')
 tape('test ', (t) => {
   const ssb = createSsb();
 
-  [...new Array(1024)].forEach(() => {
-    ssb.publish({ type: 'test' }, (err, entry) => {
-      t.error(err)
-      t.ok(entry)
-    })
+  // We write 7919 messages, which should be bigger than any cache. It's also a
+  // prime number and shouldn't line up perfectly with any batch sizes.
+  const messages = 7919
+
+  const assertsPerMessage = 4;
+  t.plan(messages * assertsPerMessage);
+
+  Promise.all([...new Array(messages)].map(async (_, i) => {
+    const entry = await promisify(ssb.publish)({ type: 'test' })
+    t.equal(typeof entry, 'object')
+    t.equal(typeof entry.key, 'string')
+    t.equal(typeof entry.value, 'object')
+    t.equal(entry.value.sequence, i + 1)
+  })).then(() => {
+    ssb.close(t.end)
   })
 })
 
